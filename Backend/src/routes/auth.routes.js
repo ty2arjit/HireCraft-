a3e426a5537ee84adcc88749d249203a29f1749a
ty2arjit <ty2arjit@gmail.com>
2026-03-30 02:10:14 +0530

    feat : All routes for login

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,36 @@
+const { Router } = require('express');
+const authController = require('../controllers/auth.controller');
+
+const authRouter = Router();
+
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user with username, email, and password
+ * @access Public
+ */
+authRouter.post("/register", authController.registerUserController);
+
+/**
+ * @route POST /api/auth/login
+ * @desc Login a user with email and password, returns a JWT token in a cookie
+ * @access Public
+ */
+authRouter.post("/login", loginUserController.loginUserController);
+
+/**
+ * @route POST /api/auth/logout
+ * @desc Logout a user by blacklisting the JWT token and clearing the cookie
+ * @access Public
+ */
+authRouter.get("/logout", authController.logoutUserController);
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access Private
+ */
+authRouter.get("/get-me", authMiddleware.authUser, authController.getMeController);
+
+
+
+module.exports = authRouter
