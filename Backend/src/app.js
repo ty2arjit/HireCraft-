278773c8ea2e834325dc82ff28c06d2c4bf1e946
ty2arjit <ty2arjit@gmail.com>
2026-04-01 15:13:57 +0530

    feat : added cors policy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth.routes');
 
 const app = express();
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
